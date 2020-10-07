74b3df98139cd310fcdec883e9b6ca9efaf96bbf
Erick Márquez <erick.marquez@britecore.com>
2020-10-07 14:58:51 -0700

    First commit

---
+++ scripts/gh-pages.js
@@ -0,0 +1,16 @@
+var ghpages = require('gh-pages');
+
+ghpages.publish(
+    '__sapper__/export/dsu-timer',
+    {
+        branch: 'gh-pages',
+        repo: 'https://github.com/deathwebo/dsu-timer.git',
+        user: {
+            name: 'Erick M',
+            email: 'tony3c24@gmail.com'
+        }
+    },
+    () => {
+        console.log('Deploy Complete!')
+    }
+)
